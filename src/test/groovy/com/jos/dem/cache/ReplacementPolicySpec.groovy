a96bade9924f4d752eab870f8146dba805555323
josdem <joseluis.delacruz@gmail.com>
2018-03-04 17:52:14 -0600

    Adding test FIFO replacement policy

--- src/test/groovy/com/jos/dem/cache/ReplacementPolicySpec.groovy
+++ src/test/groovy/com/jos/dem/cache/ReplacementPolicySpec.groovy
@@ -6,6 +6,13 @@ class ReplacementPolicySpec extends Specification {
 
   CacheManager cacheManager = new CacheManager()
 
+  ValidationStrategy strategy = Mock(ValidationStrategy)
+
+  def setup(){
+    strategy.isValid(_ as String) >> true
+    cacheManager.setStrategy(strategy)
+  }
+
   void "should fill cache"(){
     when:'We add 5 entries'
       cacheManager.put('josdem','developer')
@@ -16,4 +23,20 @@ class ReplacementPolicySpec extends Specification {
     then:'We expect cache is full'
      cacheManager.isFull() == true
   }
+
+  void "should add a new entity using FIFO policy"(){
+    given:'We 5 entries'
+      cacheManager.put('josdem','developer')
+      cacheManager.put('tgrip','developer')
+      cacheManager.put('skuarch','developer')
+      cacheManager.put('emoranchel','developer')
+      cacheManager.put('jeduan','developer')
+    when:'We add an extra entity'
+      cacheManager.put('josdem','developer')
+    then:'We expect 5 elements and new entity added'
+      cacheManager.isFull() == true
+      cacheManager.get('josdem') == null
+      cacheManager.get('siedrix') == 'developer'
+  }
+
 }
